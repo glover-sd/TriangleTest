070791a7e0567cb6e54b4084f32b167a46752021
glover-sd <brent.l.glover@gmail.com>
2021-09-04 11:00:38 -0700

    Initial Commit for Triangle Test. Testing UI and initial UnitTests.

---
+++ app/src/main/java/Triangle.kt
@@ -0,0 +1,10 @@
+class Triangle(val sides: List<Double>) {
+
+    val dSideAB = sides[0]
+    val dSideBC = sides[1]
+    val dSideCA = sides[2]
+
+    fun setTriangleType(list: MutableList<Double>) {
+
+    }
+}
